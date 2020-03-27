6e181691a6d4bf1e3de7d225c752743511f80126
Patrick Lai <weitanai@pinduoduo.com>
2020-03-27 15:38:56 +0800

    chore(DAO): add user dao impl

---
+++ src/main/java/com/example/demo/dao/impl/UserDao.java
@@ -0,0 +1,6 @@
+package com.example.demo.dao.impl;
+
+import com.example.demo.dao.ICRUD;
+import com.example.demo.entity.User;
+
+public class UserDao extends NormalCRUD<User> implements ICRUD<User> {}
